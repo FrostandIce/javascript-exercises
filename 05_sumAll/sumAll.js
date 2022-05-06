652baeb8c8c727431bc31390cf9ff825ca8e93d5
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-06 13:07:20 -0500

    Finished 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
+  let temp;
+  let total = 0;
+  if (a > b) {
+    temp = b;
+    b = a;
+    a = temp;
+  }
 
+  for (let i = a; i <= b; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
