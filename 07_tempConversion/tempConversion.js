1981343f96ae42c65e51721359d9a7d81bfb4727
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-06 13:30:45 -0500

    FInished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fahren) {
+  return Math.round((fahren - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (cel) {
+  return Math.round((cel * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
