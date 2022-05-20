cd4c342840abcc4ad5220d0964b13943f6674bce
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-20 11:23:07 -0500

    Done 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  b = 0;
+  return a.reduce((prevValue, currValue) => prevValue + currValue, b);
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  b = 1;
+  return a.reduce((prevValue, currValue) => prevValue * currValue, b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a <= 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
